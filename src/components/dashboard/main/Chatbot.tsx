39238476d3b09239457e6edf7fc7e719fa0b72a1
Iwansal64 <iwansal64@gmail.com>
2026-08-09 05:31:02 +0700

    [dashboard] Create ChatBot

--- src/components/dashboard/main/Chatbot.tsx
+++ src/components/dashboard/main/Chatbot.tsx
@@ -1,6 +1,33 @@
 export default function Chatbot() {
 
-        return <div className="" style={{
+        return <div className="flex flex-col" style={{
                 gridArea: "cb"
-        }}>Chatbot</div>;
+        }}>
+                {/* Title */}
+                <div className="py-2 text-center w-full">
+                        AI ChatBot
+                </div>
+                {/* Chat */}
+                <div className="grid grid-flow-row gap-4 p-2 overflow-auto w-full">
+                        <MessageBubble message_content="sdafs asmmsdfmsdakfm sd sdafsdokfmsadfsadf sdfdbmfkbcopkscvopjbvd bsdvbdbk" is_user={false} time={new Date()} />
+                        <MessageBubble message_content="TEST" is_user time={new Date()} />
+                        <MessageBubble message_content="sdafs asmmsdfmsdakfm sd sdafsdokfmsadfsadf sdfdbmfkbcopkscvopjbvd bsdvbdbk" is_user={false} time={new Date()} />
+                        <MessageBubble message_content="TEST" is_user time={new Date()} />
+                        <MessageBubble message_content="sdafs asmmsdfmsdakfm sd sdafsdokfmsadfsadf sdfdbmfkbcopkscvopjbvd bsdvbdbk" is_user={false} time={new Date()} />
+                        <MessageBubble message_content="TEST" is_user time={new Date()} />
+                        <MessageBubble message_content="sdafs asmmsdfmsdakfm sd sdafsdokfmsadfsadf sdfdbmfkbcopkscvopjbvd bsdvbdbk" is_user={false} time={new Date()} />
+                        <MessageBubble message_content="TEST" is_user time={new Date()} />
+                </div>
+                {/* Message Input */}
+                <div className="w-full p-2">
+                        <input type="text" className="p-2 rounded-2xl w-full h-full bg-gray-600" />
+                </div>
+        </div>;
+}
+
+function MessageBubble({ message_content, time, is_user }: { message_content: string, time: Date, is_user: boolean }) {
+        return <div className={`pt-4 pb-3 px-4 flex flex-col gap-2 relative rounded-2xl w-4/5 ${is_user && "place-self-end bg-gray-800"}`}>
+                <p className="whitespace-pre-wrap">{message_content}</p>
+                <p className="text-xs">{time.toLocaleDateString()}</p>
+        </div>;
 }
