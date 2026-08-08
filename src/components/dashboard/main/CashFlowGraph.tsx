bcd7ebe1e4b0680778984f30f4c765e1504a21b1
Iwansal64 <iwansal64@gmail.com>
2026-08-08 19:55:04 +0700

    [dashboard] Create Cash Flow Chart

--- src/components/dashboard/main/CashFlowGraph.tsx
+++ src/components/dashboard/main/CashFlowGraph.tsx
@@ -1,6 +1,45 @@
+'use client';
+
+import { ChartData, Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, BarElement } from "chart.js";
+import { Chart } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarElement);
+
+export const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+};
+
+
+const data: ChartData = {
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        datasets: [{
+                type: "line",
+                label: "Income (in $)",
+                data: [200, 80, 160, 130, 60, 198, 200],
+                fill: false,
+                borderColor: 'rgb(20, 200, 100)',
+                tension: 0.1
+        }, {
+                type: "line",
+                label: "Outcome (in $)",
+                data: [50, 100, 60, 60, 180, 160, 100],
+                fill: false,
+                borderColor: 'rgb(220, 20, 90)',
+                tension: 0.1
+        }, {
+                type: "bar",
+                label: "Total",
+                data: [150, 130, 230, 300, 180, 218, 318],
+                backgroundColor: 'rgba(200, 200, 200, 0.2)'
+                }],
+}
+
 export default function CashflowGraph() {
 
-        return <div className="" style={{
+        return <div className="p-4" style={{
                 gridArea: "gr"
-        }}>CashflowGraph</div>;
+        }}>
+                <Chart type="bar" data={data} options={options} className="w-full h-full" />
+        </div>;
 }
