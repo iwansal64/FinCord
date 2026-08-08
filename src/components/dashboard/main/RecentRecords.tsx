9eb50b1230c30126af7e99066918a79647e5589b
Iwansal64 <iwansal64@gmail.com>
2026-08-08 16:56:31 +0700

    [dashboard] Create Recent Records

--- src/components/dashboard/main/RecentRecords.tsx
+++ src/components/dashboard/main/RecentRecords.tsx
@@ -1,6 +1,30 @@
 export default function RecentRecords() {
 
-        return <div className="" style={{
+        return <div className="p-8 grid grid-flow-row auto-rows-36 gap-4 overflow-auto" style={{
                 gridArea: "rcd"
-        }}>RecentRecords</div>;
+        }}>
+                <Record title="Dinner with friends" date={new Date()} income={15} currency="$" />
+                <Record title="Buy a new phone" date={new Date()} income={-2000} currency="$" />
+                <Record title="Work 24 hours" date={new Date()} income={5200} currency="$" />
+                <Record title="Dinner with friends" date={new Date()} income={15} currency="$" />
+                <Record title="Buy a new phone" date={new Date()} income={-2000} currency="$" />
+                <Record title="Work 24 hours" date={new Date()} income={5200} currency="$" />
+        </div>;
+}
+
+function Record({ title, date, income, currency }: {
+        title: string,
+        date: Date,
+        income: number,
+        currency: string
+}) {
+        return <div className="w-full flex flex-col p-4 border border-white rounded-2xl">
+                <div className="flex flex-row justify-between">
+                        <p className="text-lg">{title}</p>
+                        <p className="text-sm text-gray-300">{date.toLocaleDateString()}</p>
+                </div>
+                <div className="w-full h-full flex justify-center items-center">
+                        <p className="text-4xl">{income > 0 ? `${currency}${income}` : `-${currency}${-income}`}</p>
+                </div>
+        </div>;
 }
