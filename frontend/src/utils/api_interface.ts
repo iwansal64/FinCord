6c367235af4ec279e6e4fb1c65b0be937c56f4c3
Iwansal64 <iwansal64@gmail.com>
2026-08-15 13:16:09 +0700

    [dev] More reliable API interface

--- frontend/src/utils/api_interface.ts
+++ frontend/src/utils/api_interface.ts
@@ -1,27 +1,38 @@
 import useSWRMutation from "swr/mutation";
 
+type SendPostResultType = {
+        result: Response | null,
+        error: any | null;
+};
+
 const base_url = process.env.NEXT_PUBLIC_BASE_URL;
 const sendPOST =
         <T>() =>
-        async (url: string, { arg }: { arg: T }) => {
-                try {
-                        const res = await fetch(base_url + url, {
-                                method: "POST",
-                                headers: { "Content-Type": "application/json" },
-                                body: JSON.stringify(arg),
-                                credentials: "include",
-                        });
+                async (url: string, { arg }: { arg: T; }): Promise<SendPostResultType> => {
+                        try {
+                                const res = await fetch(base_url + url, {
+                                        method: "POST",
+                                        headers: { "Content-Type": "application/json" },
+                                        body: JSON.stringify(arg),
+                                        credentials: "include",
+                                });
 
-                        return res;
-                } catch (error) {
-                        return error;
-                }
-        };
+                                return {
+                                        result: res,
+                                        error: null
+                                };
+                        } catch (error) {
+                                return {
+                                        result: null,
+                                        error: error
+                                };
+                        }
+                };
 
 export function useRegisterAPI() {
         const { trigger, data, error, isMutating } = useSWRMutation(
                 "/user/register",
-                sendPOST<{ email: string }>(),
+                sendPOST<{ email: string; }>(),
         );
         return {
                 trigger,
@@ -34,7 +45,7 @@ export function useRegisterAPI() {
 export function useLoginAPI() {
         const { trigger, data, error, isMutating } = useSWRMutation(
                 "/user/login",
-                sendPOST<{ email_or_username: string; password: string }>(),
+                sendPOST<{ email_or_username: string; password: string; }>(),
         );
         return {
                 trigger,
